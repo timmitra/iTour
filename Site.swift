b829b2c0cc9e24120ac98545e7ad901066b85703
Tim Mitra <tim@it-guy.com>
2023-10-01 01:07:31 -0400

    add site relationship

--- Site.swift
+++ Site.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model 
+class Site {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
--- iTour/Destination.swift
+++ iTour/Destination.swift
@@ -15,6 +15,7 @@ class Destination {
     var details: String
     var date: Date
     var priority: Int
+    var sites = [Site]() // empty Site array
     
     init(name: String = "", details: String = "", date: Date = .now, priority: Int = 2) {
         self.name = name
