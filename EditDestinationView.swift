bd1e332dccd368e1d003444ab7b0e0c5e50e7957
Tim Mitra <tim@it-guy.com>
2023-09-30 23:55:32 -0400

    add Edit View create in Memory config to we can make preview data

---
+++ EditDestinationView.swift
@@ -0,0 +1,18 @@
+//
+//  EditDestinationView.swift
+//  iTour
+//
+//  Created by Tim Mitra on 2023-09-30.
+//
+
+import SwiftUI
+
+struct EditDestinationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditDestinationView()
+}
