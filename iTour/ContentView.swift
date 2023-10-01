ab83a436aea3024c1438484dd031cb0e70554158
Tim Mitra <tim@it-guy.com>
2023-10-01 00:31:12 -0400

    make a DestinationListingView for sorting move the List into this view add DestinationListingView() into ContentView

--- iTour/ContentView.swift
+++ iTour/ContentView.swift
@@ -11,23 +11,11 @@ import SwiftData
 struct ContentView: View {
     
     @Environment(\.modelContext) var modelContext
-    @Query var destinations: [Destination]
     @State private var path = [Destination]()
     
     var body: some View {
         NavigationStack(path: $path) {
-            List {
-                ForEach(destinations) { destination in
-                    NavigationLink(value: destination) {
-                        VStack(alignment: .leading) {
-                            Text(destination.name)
-                                .font(.headline)
-                            Text(destination.date.formatted(date: .long, time: .shortened))
-                        }
-                    }
-                }
-                .onDelete(perform: deleteDestinations) // swipe to delete
-            }
+            DestinationListingView()
             .navigationTitle("iTour")
             .navigationDestination(for: Destination.self, destination: EditDestinationView.init)
             .toolbar{
@@ -54,12 +42,6 @@ struct ContentView: View {
         path = [destination] // show this with the navigation
     }
     
-    func deleteDestinations(_ indexSet: IndexSet) {
-        for index in indexSet {
-            let destination = destinations[index]
-            modelContext.delete(destination)
-        }
-    }
 }
 
 #Preview {
---
+++ iTour/DestinationListingView.swift
@@ -0,0 +1,42 @@
+//
+//  DestinationListingView.swift
+//  iTour
+//
+//  Created by Tim Mitra on 2023-10-01.
+//
+
+import SwiftUI
+import SwiftData
+
+struct DestinationListingView: View {
+    
+    @Environment(\.modelContext) var modelContext
+    @Query var destinations: [Destination]
+    
+    var body: some View {
+        List {
+            ForEach(destinations) { destination in
+                NavigationLink(value: destination) {
+                    VStack(alignment: .leading) {
+                        Text(destination.name)
+                            .font(.headline)
+                        Text(destination.date.formatted(date: .long, time: .shortened))
+                    }
+                }
+            }
+            .onDelete(perform: deleteDestinations) // swipe to delete
+        }
+    }
+    
+    func deleteDestinations(_ indexSet: IndexSet) {
+        for index in indexSet {
+            let destination = destinations[index]
+            modelContext.delete(destination)
+        }
+    }
+
+}
+
+#Preview {
+    DestinationListingView()
+}
