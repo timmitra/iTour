f731c785a24f0d1912445cec9db9afe411e8138c
Tim Mitra <tim@it-guy.com>
2023-09-30 22:58:57 -0400

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Tim Mitra on 2023-09-30.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
