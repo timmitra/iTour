0c1eca3990474bd5e77527cf5935ed1061f47316
Tim Mitra <tim@it-guy.com>
2023-09-30 23:55:54 -0400

    de edit deatil view

--- EditDestinationView.swift
+++ EditDestinationView.swift
@@ -6,13 +6,27 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct EditDestinationView: View {
+    
+    @Bindable var destination: Destination
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    EditDestinationView()
+    do {
+        // need config & container to feed the data
+        let config = ModelConfiguration(isStoredInMemoryOnly: true)
+        let container = try ModelContainer(for: Destination.self, configurations: config)
+        let example = Destination(name: "Example Destination", details: "Expample details go here and expand automatically as they are edited.")
+        
+    return EditDestinationView(destination: example)
+            .modelContainer(container)
+    } catch {
+            fatalError("Failed to create model")
+        }
 }
